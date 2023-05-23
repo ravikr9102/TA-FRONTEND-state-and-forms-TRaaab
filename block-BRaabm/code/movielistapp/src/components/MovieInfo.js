1f4cf1d073d08096d1caa03ca072e9a57b6180a4
Ravi kumar <rkravikr9102@gmail.com>
2023-05-23 17:49:32 +0530

    refactor

--- block-BRaabm/code/movielistapp/src/components/MovieInfo.js
+++ block-BRaabm/code/movielistapp/src/components/MovieInfo.js
@@ -1,17 +1,39 @@
-function MovieInfo(props) {
-  return (
-    <>
-    <div className="bg">
-    <h4>{props.moreInfo.Actors}</h4>
-      <figure>
-        {props.moreInfo.Images.map((img) => (
-          <img className="info-img" src={img} alt="" />
-        ))}
-      </figure>
-      <button>X</button>
-    </div>
-    </>
-  );
-}
+import React from 'react';
+
+class MovieInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisplay: true,
+    };
+  }
+  handleClose = () => {
+    this.setState({
+      isDisplay: !this.state.isDisplay,
+    });
+  };
+  render() {
+    return (
+      <>
+        {this.state.isDisplay ? (
+          <div className="bg">
+            <h4>{this.props.moreInfo.Actors}</h4>
+            <figure>
+              {this.props.moreInfo.Images.map((img) => (
+                <img key={img} className="info-img" src={img} alt="" />
+              ))}
+            </figure>
+            <p>{this.props.moreInfo.Genre}</p>
+            <p>{this.props.moreInfo.Awards}</p>
+            <p>{this.props.moreInfo.Language}</p>
+            <button onClick={this.handleClose}>X</button>
+          </div>
+        ) : (
+          ''
+        )}
+      </>
+    );
+  }
+} 
 
 export default MovieInfo;
