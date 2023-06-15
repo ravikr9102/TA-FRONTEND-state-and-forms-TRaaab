faf0f60d81368886a6d082ae4f5555d1c4a4c591
Ravi kumar <rkravikr9102@gmail.com>
2023-06-15 10:52:40 +0530

    added some functionality

--- block-BRaabw/shopping-cart/src/components/Cart.js
+++ block-BRaabw/shopping-cart/src/components/Cart.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 class Cart extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       isVisible: false,
       quantity: 0,
